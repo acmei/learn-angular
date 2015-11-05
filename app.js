11a3b7efec263a8c253868714db0d74b188d1223
Anita <asovay@gmail.com>
2015-11-05 13:56:52 -0800

    done through section 3

--- app.js
+++ app.js
@@ -5,19 +5,81 @@
     this.products = gems;
   });
 
+  app.controller('PanelController', function(){
+    this.tab = 1;
+
+    this.selectTab = function(setTab) {
+      this.tab = setTab;
+    };
+
+    this.isSelected = function(checkTab) {
+      return this.tab === checkTab;
+    };
+  });
+
+  app.controller('GalleryController', function(){
+    this.current = 0;
+
+    this.setCurrent = function(value){
+      this.current = value || 0;
+    }
+  });
+
+  app.controller('ReviewController', function(){
+    this.review = {};
+
+    this.addReview = function(product){
+      product.reviews.push(this.review);
+      this.review = {};
+    };
+  });
+
   var gems = [
     {
       name: 'Dodecahedron',
-      price: 2.95,
+      price: 2.00,
       description: 'Some gems have hidden qualities beyond their luster, beyond their shine...Dodeca is one of those gems.',
       canPurchase: true,
-      soldOut: false
+      soldOut: false,
+      images: [
+        {
+          full: 'dodecahedron-01-full.jpg',
+          thumb: 'dodecahedron-01-thumb.jpg'
+        },
+        {
+          full: 'dodecahedron-02-full.jpg',
+          thumb: 'dodecahedron-02-thumb.jpg'
+        }
+      ],
+      reviews: [
+        {
+          stars: 5,
+          body: "I love this product!",
+          author: "joe@thomas.com"
+        },
+        {
+          stars: 1,
+          body: "This product sucks",
+          author: "tim@hater.com"
+        }
+      ]
     },
     {
       name: "Pentagonal Gem",
       price: 5.95,
       description: "Origin of the Pentagonal Gem is unknown, hence its low value. It has a very high shine and 12 sides.",
-      canPurchase: false
+      canPurchase: false,
+      soldOut: false,
+      images: [
+        {
+          full: 'pentagonal-01-full.jpg',
+          thumb: 'pentagonal-01-thumb.jpg'
+        },
+        {
+          full: 'pentagonal-02-full.jpg',
+          thumb: 'pentagonal-02-thumb.jpg'
+        }
+      ]
     }
   ];
 
